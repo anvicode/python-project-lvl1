8cec740549b687fec8e52e0bd9801d3b28e591b9
Andrey Vorontsov <reygofor@gmail.com>
2022-07-05 18:19:56 +0300

    chore: add step 4

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,9 @@
+"""Add cli."""
+
 import prompt
 
+
 def welcome_user():
+    """Add welcome User."""
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+"""Add brain_games."""
+
 from brain_games.cli import welcome_user
 
+
 def main():
+    """Add main."""
     print('Welcome to the Brain Games!')
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
